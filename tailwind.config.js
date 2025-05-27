2bdad2cdcff69db2bce3bc9cdbcc058c5b207827
Ali <alimadadi110@gmail.com>
2025-05-27 09:07:05 +0330

    Fix: install tailwindcss and config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'vazir': ['Vazir', 'sans-serif'],
+        'sols': ['Sols', 'serif'],
+      },
+    },
   },
   plugins: [],
 } 
