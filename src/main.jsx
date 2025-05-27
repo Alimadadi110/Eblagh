bb40e8ae698f3d6014125b8beb1b8e0355a2ecc2
Ali <alimadadi110@gmail.com>
2025-05-27 08:36:16 +0330

    CSS integrated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import '@css'
-import LoginPage from '@components/LoginPage'
+import './styles/index.css'
+import LoginPage from './components/LoginPage'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
